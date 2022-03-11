5a711c34e4c1e1dc3ab41ac1643a707a0a0a80e9
陈帆 <1490040662@qq.com>
2022-03-11 09:01:11 +0800

    fix

--- index.js
+++ index.js
@@ -93,15 +93,15 @@
  
    // if brotli === true && !(await exists(path + '.br')) compress the file
    if (ctx.acceptsEncodings('br', 'identity') === 'br' && brotli && !(await exists(path + '.br'))) {
+     console.log(1111);
      const br = zlib.createBrotliCompress()
-     const rs = fs.createReadStream(path)
-     const ws = fs.createWriteStream(path + '.br')
+     const rs =  fs.createReadStream(path)
+     const ws =  fs.createWriteStream(path + '.br')
      rs.pipe(br).pipe(ws)
-   }
-   if (ctx.acceptsEncodings('gzip', 'identity') === 'gzip' && gzip && !(await exists(path + '.gz'))) {
-     const gz = zlib.createBrotliCompress()
-     const rs = fs.createReadStream(path)
-     const ws = fs.createWriteStream(path + '.gz')
+   } else if (ctx.acceptsEncodings('gzip', 'identity') === 'gzip' && gzip && !(await exists(path + '.gz'))) {
+     const gz = zlib.createGzip()
+     const rs =  fs.createReadStream(path)
+     const ws =  fs.createWriteStream(path + '.gz')
      rs.pipe(gz).pipe(ws)
    }
    
